2425c7d45a2d2c9330ea25213ab7cdd5694ee600
daniil <savindaniil29@gmail.com>
2021-03-14 19:03:42 +0300

    1

--- WhoU/WhoU/settings.py
+++ WhoU/WhoU/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,6 +71,25 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'WhoU.wsgi.application'
+
+
+# CACHES = [
+#     {
+#         IMAGEFIT_CACHE_BACKEND_NAME: {
+#             'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#     }
+#     }
+# ]
+SESSION_COOKIE_HTTPONLY = True
+
+#
+# SESSION_ENGINE = [
+#     {
+#         "django.contrib.sessions.backends.cache",
+#         "django.contrib.sessions.backends.file",
+#         "django.contrib.sessions.backends.signed_cookies",
+#     }
+# ]
 
 
 # Database
--- WhoU/firstapp/views.py
+++ WhoU/firstapp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
 import pyrebase
+from requests.exceptions import HTTPError
+from django.contrib.auth.forms import UserCreationForm
+global Info
+global Id
 
 
 def index_profile(request):
-    print(request.method)
     if request.method == "POST":
-        print(request.POST)
+        print(Info)
         return render(request, "whou_profile.html")
 
     return render(request, "whou_profile.html")
@@ -24,15 +27,29 @@ def index_meetings(request):
 
 def index_signin(request):
     userform = UserForm()
-    return render(request, "whou_sign_in.html")
+    if request.method == "POST":
+        postsignin(request)
+    context = {}
+    print(context)
+    if request.POST.get('email') is not None:
+        context['email'] = request.POST.get('email')
+
+    return render(request, "whou_sign_in.html", context)
 
 
 def index_signup(request):
-    userform = UserForm()
-    return render(request, "whou_sign_up.html")
+    if request.method == "POST":
+        postsignup(request)
+    context = {}
+    if request.POST.get('email') is not None:
+        context['email'] = request.POST.get('email')
+    return render(request, "whou_sign_up.html", context)
 
 
 def index_main(request):
+    # from pprint import pprint
+    # request.session['user_data'] = {}
+    # pprint(dict(request.session))
     userform = UserForm()
     return render(request, "whou_main.html")
 
@@ -41,40 +58,71 @@ def index_updates(request):
     userform = UserForm()
     return render(request, "whou_updates.html")
 
-config = {
-    'apiKey': "AIzaSyBSxWNN_VKfsSTwCp3yjoe3x_W0RjNivdw",
-    'authDomain': "whou-d361d.firebaseapp.com",
-    'databaseURL': "https://whou-d361d.firebaseio.com",
-    'projectId': "whou-d361d",
-    'storageBucket': "whou-d361d.appspot.com",
-    'messagingSenderId': "994830820246",
-    'appId': "1:994830820246:web:94eecba0e57f35704673b7",
-    'measurementId': "G-SY3JH2ZEKQ"
-  }
-
-firebase = pyrebase.initialize_app(config)
-
-auth = firebase.auth()
-
-def SignIn(request):
-    return redirect('signin/')
+# def SignIn(request):
+#     return redirect('signin/')
 
 
 def postsignin(request):
+    Info = None
+    Id = None
+    # print(request)
     email = request.POST.get('email')
+    print(email)
     passw = request.POST.get('password')
-    db = firebase.database()
-    db.child( "users" ).child( "Morty" )
-    user = auth.sign_in_with_email_and_password(email, passw)
-    data = {
-        "name": "Mortimer 'Morty' Smith"
+    print(passw)
+    config = {
+        'apiKey': "AIzaSyBSxWNN_VKfsSTwCp3yjoe3x_W0RjNivdw",
+        'authDomain': "whou-d361d.firebaseapp.com",
+        'databaseURL': "https://whou-d361d.firebaseio.com",
+        'projectId': "whou-d361d",
+        'storageBucket': "whou-d361d.appspot.com",
+        'messagingSenderId': "994830820246",
+        'appId': "1:994830820246:web:94eecba0e57f35704673b7",
+        'measurementId': "G-SY3JH2ZEKQ"
     }
-    user = auth.refresh( user['refreshToken'] )
-    token = auth.create_custom_token("your_custom_id")
-    user = auth.sign_in_with_custom_token( token )
-    if request.method == "POST":
-        print(request.POST)
-    return redirect('/profile/')
+    firebase = pyrebase.initialize_app(config)
+    auth = firebase.auth()
+    try:
+        user = auth.sign_in_with_email_and_password(email, passw)
+        # auth.send_email_verification(user['idToken'])
+        # auth.send_password_reset_email(email)
+        # print(auth.get_account_info(user['idToken']))
+
+        Info = auth.get_account_info(user['idToken'])['users']
+        Id  = Info[0]['localId']
+        print(Id)
+        print(Info)
+
+        # print(user, type(user))
+        # print(db.child('Users').get('name'))
+    except HTTPError as error:
+        print(error)
+    print("Работает")
+
+def postsignup(request):
+    config = {
+        'apiKey': "AIzaSyBSxWNN_VKfsSTwCp3yjoe3x_W0RjNivdw",
+        'authDomain': "whou-d361d.firebaseapp.com",
+        'databaseURL': "https://whou-d361d.firebaseio.com",
+        'projectId': "whou-d361d",
+        'storageBucket': "whou-d361d.appspot.com",
+        'messagingSenderId': "994830820246",
+        'appId': "1:994830820246:web:94eecba0e57f35704673b7",
+        'measurementId': "G-SY3JH2ZEKQ"
+    }
+
+
+    data = {
+        'email': request.POST.get('email'),
+        'pass': request.POST.get('password'),
+        'name': request.POST.get('name'),
+        'phone': request.POST.get('phone'),
+    }
+
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+    db.child('Users').child('qwerty').set(data)
+    print('Работаеттттттттт')
 
 
 
